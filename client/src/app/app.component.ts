13b6764b13e28cbcd04f8837830cfb820d026406
Dean Kerr <dean_k@live.co.uk>
2016-12-08 14:53:07 +0000

    Add top-level app styles to app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'app',
+    encapsulation: ViewEncapsulation.None,
+    styleUrls: ['./app.css'],
     templateUrl: './app.html'
 })
 export class AppComponent {
