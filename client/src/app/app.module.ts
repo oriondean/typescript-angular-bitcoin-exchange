12a258316538b6662b9bf126ab451135b11cfcf9
Dean Kerr <dean_k@live.co.uk>
2016-12-08 14:54:02 +0000

    Updated app module to provide account and socket services

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { OrderEntryComponent } from './order-entry/order-entry.component';
+import { SocketService } from "./socket.service";
+import { AccountService } from "./account.service";
 
 @NgModule({
     bootstrap: [AppComponent],
     declarations: [AppComponent, OrderEntryComponent],
-    imports: [BrowserModule, FormsModule]
+    imports: [BrowserModule, FormsModule],
+    providers: [SocketService, AccountService]
 })
 export class AppModule {}
